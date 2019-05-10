c938878b3503012fd05b9740015447d3a553f493
Vengeruk <zwolfen85@gmail.com>
2019-05-11 01:01:02 +0600

    section 1.4.4 ex. 1.17-1.19

--- ConsoleApplication1/ConsoleApplication1/main.cpp
+++ ConsoleApplication1/ConsoleApplication1/main.cpp
@@ -8,10 +8,27 @@ int main()
 	//setlocale(LC_CTYPE, "rus");
 	system("chcp 1251>nul");
 
-	cout << "ex 1.16" << endl;
-	while (std::cin >> value)
-		sum += value;
-	cout << "Sum is: " << sum << endl;
+	cout << "ex 1.17 1.18 1.19" << endl;
+	// if all values are equal then get message like 'value' occurs 'n' times after eof character
+	// if no equal values then get message like "'value' occurs '1' times" for each value after eof character
+	int currVal = 0, val = 0;
+	// read first number and ensure that we have data to process
+	if (std::cin >> currVal) {
+		int cnt = 1; // store the count for the current value we're processing
+		while (std::cin >> val) { // read the remaining numbers
+			if (val == currVal) // if the values are the same
+				++cnt; // add 1 to cnt
+			else { // otherwise, print the count for the previous value
+				std::cout << currVal << " occurs "
+					<< cnt << " times" << std::endl;
+				currVal = val; // remember the new value
+				cnt = 1; // reset the counter
+			}
+		} // while loop ends here
+		// remember to print the count for the last value in the file
+		std::cout << currVal << " occurs "
+			<< cnt << " times" << std::endl;
+	} // outermost if statement ends here
 
 	//system("pause>nul");
 	return 0;
