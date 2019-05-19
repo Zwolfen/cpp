980fdce13b0a02d03bf80bc6094cffc8ab3015bf
Vengeruk <zwolfen85@gmail.com>
2019-05-20 00:42:50 +0600

    Section 1.5.1 ex. 1.20 - 1.22

--- ConsoleApplication1/ConsoleApplication1/main.cpp
+++ ConsoleApplication1/ConsoleApplication1/main.cpp
@@ -1,35 +1,43 @@
 #include<iostream>
+#include "Sales_item.h"
+
 using namespace std;
 int main()
 {
-	int value;
-	int i;
-	int sum = 0;
 	//setlocale(LC_CTYPE, "rus");
 	system("chcp 1251>nul");
 
-	cout << "ex 1.17 1.18 1.19" << endl;
-	// if all values are equal then get message like 'value' occurs 'n' times after eof character
-	// if no equal values then get message like "'value' occurs '1' times" for each value after eof character
-	int currVal = 0, val = 0;
-	// read first number and ensure that we have data to process
-	if (std::cin >> currVal) {
-		int cnt = 1; // store the count for the current value we're processing
-		while (std::cin >> val) { // read the remaining numbers
-			if (val == currVal) // if the values are the same
-				++cnt; // add 1 to cnt
-			else { // otherwise, print the count for the previous value
-				std::cout << currVal << " occurs "
-					<< cnt << " times" << std::endl;
-				currVal = val; // remember the new value
-				cnt = 1; // reset the counter
-			}
-		} // while loop ends here
-		// remember to print the count for the last value in the file
-		std::cout << currVal << " occurs "
-			<< cnt << " times" << std::endl;
-	} // outermost if statement ends here
 
+	cout << "ex 1.20" << endl;
+	Sales_item item;
+	// need to coment #include "version test.h" in Sales_item.h, else compile error
+
+	// in while loop reading and out on console
+	while (cin >> item) {
+		cout << item << endl; // print their sum
+	}
+
+	cout << "ex 1.21" << endl;
+	// need to coment #include "version test.h" in Sales_item.h, else compile error
+
+	Sales_item item1, item2;
+	cin >> item1 >> item2; // read a pair of transactions
+	if (compareIsbn(item1, item2)) {
+		cout << item1 + item2 << endl; // print their sum
+	}
+
+	cout << "ex 1.22" << endl;
+	// need to coment #include "version test.h" in Sales_item.h, else compile error
+
+	Sales_item sum, item3;
+	// making first value to sum object
+	cin >> sum;
+	while (cin >> item3) {
+		if (compareIsbn(sum, item3)) {
+			sum += item3;
+		}
+	}
+	cout << sum << endl; // print their sum
 	//system("pause>nul");
 	return 0;
 }
